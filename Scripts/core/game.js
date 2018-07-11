c1fbec89ce9264bf5dd21d1f2ae71d7dd075a7c4
Dipesh <dgoswam6@centennailcollege.ca>
2018-07-11 14:23:53 -0400

    addes npm support

---
+++ Scripts/core/game.js
@@ -0,0 +1,19 @@
+console.log("Hello");
+(function(){
+    function Init(){
+            console.log("Intializaling Stated");
+    }
+
+    function Start(){
+
+    }
+
+    function Update (){
+
+    }
+
+    function Main(){
+
+    }
+
+})
